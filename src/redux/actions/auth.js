1dbc9beac2132a0d25d85f48ffc4afb5d65499f4
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-08-19 16:21:10 +0530

    Auth State Listener created

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,5 +1,32 @@
 import firebase from "firebase";
 require("firebase/firebase-auth");
+import { USER_STATE_CHANGE } from "../constants";
+
+export const userAuthStateListener = () => (dispatch) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      dispatch(getCurrentUserData());
+    } else {
+      dispatch({ type: USER_STATE_CHANGE, currentUser: null, loaded: true });
+    }
+  });
+};
+
+export const getCurrentUserData = () => {
+  firebase
+    .firestore()
+    .collection("user")
+    .doc(firebase.auth().currentUser.uid)
+    .onSnapshot((res) => {
+      if (res.exists) {
+        return dispatch({
+          type: USER_STATE_CHANGE,
+          currentUser: res.data,
+          loaded: true,
+        });
+      }
+    });
+};
 
 export const login = (email, password) => (dispatch) =>
   new Promise((resolve, reject) => {
---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,20 @@
+import { USER_STATE_CHANGE } from "../constants";
+
+const initialState = {
+  currentUser: null,
+  loaded: false,
+  posts: [],
+};
+
+export const auth = (state = initialState, action) => {
+  switch (action.type) {
+    case USER_STATE_CHANGE:
+      return {
+        ...state,
+        currentUser: action.currentUser,
+        loaded: action.loaded,
+      };
+    default:
+      return state;
+  }
+};
